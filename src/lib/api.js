d48f860ffe2ac1c7ae089faa4b34b80d75dcf70d
Blackstar <baker644@umn.edu>
2024-03-24 10:31:43 -0500

    Changed to Node backend?

--- src/lib/api.js
+++ src/lib/api.js
@@ -35,49 +35,15 @@ export async function getGeocode(address) {
 }
 
 export async function getState(crds){
-  const url = `https://www.gis.lcc.mn.gov/iMaps/districts/php/getPointData.php?lat=${crds.y}&lng=${crds.x}`
+  const url = new URL('/api/reps');
+  url.searchParams.append('x', crds.x);
+  url.searchParams.append('y', crds.y);
+
   try {
-    const res = await fetch(url, {
-      mode: 'cors',
-      headers: new Headers({
-        'Access-Control-Allow-Origin': 'http://localhost:5173/',
-        'Access-Control-Allow-Credentials': 'true'
-      })
-    });
+    await fetch(url, {
+      method: 'GET'
+    })
 
-    if (!res.ok) {
-      switch (res.status) {
-        case 400: /* Handle */ break;
-        case 401: /* Handle */ break;
-        case 404: /* Handle */ break;
-        case 500: /* Handle */ break;
-      }
-      throw new Error("Network response was not OK");
-    }
-
-    const type = await res.headers.get("content-type");
-    if (!type || !type.includes("application/json")) {
-      throw new TypeError("Oops, we haven't got JSON!");
-    }
-
-    const data = await res.json()
-
-    if (data?.features && data?.features[0]) {
-      // return the two candidates with info populated.
-      const repDetail = (rep, position) => { return {
-        name: rep.name,
-        position: "MN " + position,
-        district: "District " + rep.district,
-        image: `https://www.gis.lcc.mn.gov/iMaps/districts/images/${position}/${rep.district}`,
-        url: `https://www.house.mn.gov/members/profile/${rep.memid}`
-      }}
-      const stateReps = {
-        stateHouse: repDetail(data?.features[0].properties, "House"),
-        stateSenate: repDetail(data?.features[1].properties, "Senate"),
-      }
-      console.log("State Reps: ", stateReps);
-      return stateReps;
-    }
   } catch (error) {
     console.error("There has been a problem with your fetch operation:", error);
   }
---
+++ src/routes/api/reps/+server.js
@@ -0,0 +1,49 @@
+import { json } from '@sveltejs/kit'
+
+export async function GET({ clientUrl }){
+
+  console.log(clientUrl.searchParams)
+  const y = clientUrl.searchParams.get('y');
+  const x = clientUrl.searchParams.get('x');
+  const serverUrl = `https://www.gis.lcc.mn.gov/iMaps/districts/php/getPointData.php?lat=${y}&lng=${x}`
+
+  try {
+    const res = await fetch(serverUrl);
+
+    if (!res.ok) {
+      switch (res.status) {
+        case 400: /* Handle */ break;
+        case 401: /* Handle */ break;
+        case 404: /* Handle */ break;
+        case 500: /* Handle */ break;
+      }
+      throw new Error("Network response was not OK");
+    }
+
+    const type = await res.headers.get("content-type");
+    if (!type || !type.includes("application/json")) {
+      throw new TypeError("Oops, we haven't got JSON!");
+    }
+
+    const data = await res.json()
+
+    if (data?.features && data?.features[0]) {
+      // return the two candidates with info populated.
+      const repDetail = (rep, position) => { return {
+        name: rep.name,
+        position: "MN " + position,
+        district: "District " + rep.district,
+        image: `https://www.gis.lcc.mn.gov/iMaps/districts/images/${position}/${rep.district}`,
+        url: `https://www.house.mn.gov/members/profile/${rep.memid}`
+      }}
+      const stateReps = {
+        stateHouse: repDetail(data?.features[0].properties, "House"),
+        stateSenate: repDetail(data?.features[1].properties, "Senate"),
+      }
+      console.log("State Reps: ", stateReps);
+      return json(stateReps);
+    }
+  } catch (error) {
+    console.error("There has been a server problem with your fetch operation:", error);
+  }
+}
